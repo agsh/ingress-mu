6760f251b7a4dc3f23c679e05024bc48f73f7b53
Andrew D.Laptev <a.d.laptev@gmail.com>
2020-04-19 23:23:54 +0300

    `dateTime` unique index instead of wrong `geometry` index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 // eslint-disable-next-line import/no-unresolved
-import { GeoJSON } from 'geojson';
+import { Feature, GeoJSON } from 'geojson';
 import cors from 'cors';
 
 const port = 13666;
@@ -20,21 +20,51 @@ app.use(express.static(__dirname));
     type: string;
     properties: object;
     geometry: GeoJSON;
+    dateTime: Date;
   }>('mu');
-  await mu.createIndex({ geometry: '2dsphere' }, { unique: true });
+  await mu.createIndex({ geometry: '2dsphere' });
+  await mu.deleteMany({ 'properties.dateTime': null });
+  await mu.createIndex({ 'properties.dateTime': 1 }, { unique: true });
 
   app.post('/mu', async (req, res) => {
     console.log(req.body);
+    const feature = req.body;
     try {
-      await mu.insertOne(req.body);
+      await mu.insertOne({
+        ...feature,
+        properties: {
+          ...feature.properties,
+          dateTime: new Date(feature.properties!.dateTime),
+        },
+      });
     } catch (e) {
       // polygon already exists
+      console.error(e);
       return res.send({ ok: false });
     }
     return res.send({ ok: true });
   });
   app.get('/mu', async (_req, res) => {
-    const fields = await mu.find({}).toArray();
+    const fields = await mu
+      .find({
+        geometry: {
+          $geoWithin: {
+            $geometry: {
+              type: 'Polygon',
+              coordinates: [
+                [
+                  [28.157958984375, 59.25464954448365],
+                  [32.14599609375, 59.25464954448365],
+                  [32.14599609375, 60.71619779357714],
+                  [28.157958984375, 60.71619779357714],
+                  [28.157958984375, 59.25464954448365],
+                ],
+              ],
+            },
+          },
+        },
+      })
+      .toArray();
     res.send({
       type: 'FeatureCollection',
       features: fields,
