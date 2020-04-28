974de0b8ce6432ad406a7d65e8d0d2fd4844ab58
Andrew D.Laptev <a.d.laptev@gmail.com>
2020-04-29 02:09:38 +0300

    Heroku build

--- build/index.js
+++ build/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const mongodb_1 = require("mongodb");
 const cors_1 = __importDefault(require("cors"));
-const port = 13666;
+const port = process.env.PORT || 5000;
 const app = express_1.default();
 app.use(body_parser_1.default.json());
 app.use(cors_1.default());
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { MongoClient } from 'mongodb';
 import { Feature, GeoJSON } from 'geojson';
 import cors from 'cors';
 
-const port = 13666;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
